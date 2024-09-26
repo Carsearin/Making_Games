98ad5ab5b91147f2efcf6febe0d6d6ffc12e98ac
Carsearin <thuleanperspectif@gmail.com>
2024-09-27 00:35:50 +0200

    Drawing tiles for Slide Puzzle

--- Makinggames_vol_4_Slide_Puzzle/Slide_Puzzle.py
+++ Makinggames_vol_4_Slide_Puzzle/Slide_Puzzle.py
@@ -197,3 +197,13 @@ def getSpotClicked(board, x, y):
             if tileRect.collidepoint(x, y):
                 return (tileX, tileY)
     return (None, None)
+
+def drawTile(tilex, tiley, number, adjx=0, adjy=0):
+    # draw a tile at board coordinates tilex and tiley, optionally a few
+    # pixels over (determined by adjx and adjy)
+    left, top = getLeftTopOfTile(tilex, tiley)
+    pygame.draw.rect(DISPLAYSURF, TILECOLOR, (left + adjx, top + adjy, TILESIZE, TILESIZE))
+    textSurf = BASICFONT.render(str(number), True, TEXTCOLOR)
+    textRect = textSurf.get_rect()
+    textRect.center = left + int(TILESIZE / 2) + adjx, top + int(TILESIZE / 2) + adjy
+    DISPLAYSURF.blit(textSurf, textRect)
