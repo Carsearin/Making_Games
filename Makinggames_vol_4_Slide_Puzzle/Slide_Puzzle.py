2a93247ae1647fb094141e7513e666a04cf561b4
Carsearin <thuleanperspectif@gmail.com>
2024-09-27 00:31:50 +0200

    Converting from pixel coordinates to board for Puzzle Slide

--- Makinggames_vol_4_Slide_Puzzle/Slide_Puzzle.py
+++ Makinggames_vol_4_Slide_Puzzle/Slide_Puzzle.py
@@ -187,3 +187,13 @@ def getLeftTopOfTile(tileX, tileY):
     left = XMARGIN + (tileX * TILESIZE) + (tileX - 1)
     top = YMARGIN + (tileY * TILESIZE) + (tileY - 1)
     return (left, top)
+
+def getSpotClicked(board, x, y):
+    # from the x & y pixel coordinates, get the x & y board coordinates
+    for tileX in range(len(board)):
+        for tileY in range(len(board[0])):
+            left, top = getLeftTopOfTile(tileX, tileY)
+            tileRect = pygame.Rect(left, top, TILESIZE, TILESIZE)
+            if tileRect.collidepoint(x, y):
+                return (tileX, tileY)
+    return (None, None)
