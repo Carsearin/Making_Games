ed842cb603bf1d9546c76872dcc5935a57ab09d7
Carsearin <thuleanperspectif@gmail.com>
2024-09-27 00:08:48 +0200

    Defining starting board for Slide Puzzle

--- Makinggames_vol_4_Slide_Puzzle/Slide_Puzzle.py
+++ Makinggames_vol_4_Slide_Puzzle/Slide_Puzzle.py
@@ -121,3 +121,20 @@ def checkForQuit():
         if event.key == K_ESCAPE:
             terminate() # terminate if the KEYUP event was for the Esc key
         pygame.event.post(event) # put the other KEYUP event objects back
+
+def getStartingBoard():
+    # Return a board data structure with tiles in the solved state.
+    # For example, if BOARDWIDTH and BOARDHEIGHT are both 3, this function
+    # returns [[1, 4, 7], [2, 5, 8], [3, 6, BLANK]]
+    counter = 1
+    board = []
+    for x in range(BOARDWIDTH):
+        column = []
+        for y in range(BOARDHEIGHT):
+            column.append(counter)
+            counter += BOARDWIDTH
+        board.append(column)
+        counter -= BOARDWIDTH * (BOARDHEIGHT - 1) + BOARDWIDTH - 1
+
+    board[BOARDWIDTH-1][BOARDHEIGHT-1] = BLANK
+    return board
