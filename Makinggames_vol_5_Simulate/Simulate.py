9e3adc9e787734371a22441c1b771f22873116a4
Carsearin <thuleanperspectif@gmail.com>
2024-09-27 21:06:24 +0200

    Animating button flash for Simulate

--- Makinggames_vol_5_Simulate/Simulate.py
+++ Makinggames_vol_5_Simulate/Simulate.py
@@ -161,3 +161,18 @@ def flashButtonAnimation(color, animationSpeed=50):
         sound = BEEP4
         flashColor = BRIGHTGREEN
         rectangle = GREENRECT
+
+    origSurf = DISPLAYSURF.copy()
+    flashSurf = pygame.Surface((BUTTONSIZE, BUTTONSIZE))
+    flashSurf = flashSurf.convert_alpha()
+    r, g, b = flashColor
+    sound.play()
+    for start, end, step in ((0, 255, 1), (255, 0, -1)): # animation loop
+        for alpha in range(start, end, animationSpeed * step):
+            checkForQuit()
+            DISPLAYSURF.blit(origSurf, (0, 0))
+            flashSurf.fill((r, g, b, alpha))
+            DISPLAYSURF.blit(flashSurf, rectangle.topleft)
+            pygame.display.update()
+            FPSCLOCK.tick(FPS)
+    DISPLAYSURF.blit(origSurf, (0, 0))
